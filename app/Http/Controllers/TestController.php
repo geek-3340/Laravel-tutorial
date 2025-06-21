95123c529df2970194ae285a9b34d63501e83327
geek-3340 <crew.sento0864@gmail.com>
2025-06-21 12:31:14 +0900

    更新

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,11 +4,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User; // Userモデルを使用する
 
 class TestController extends Controller
 {
     // 以下のコード以外は、Laravelの標準的なコントローラーの構造でsailコマンドで作成時に自動生成されるものです。
+    // 以下はtestメソッドで、Userモデルを使用してデータベースから全ユーザーを取得し、'test'ビューに渡す処理を行います。
     public function test(){
-        return view('test');
+        $users=User::all();
+        return view('test',compact('users')); // 'test'ビューにユーザーデータを渡す
     }
 }
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,3 +1,15 @@
 {{-- app/Http/Controllers/TestControllerから参照 --}}
 {{-- 以下を表示 --}}
 Hello World!
+<p>ーーーーーーーーーーー</p>
+{{--
+    TestControllerから渡された全ユーザーデータを元に
+    foreachを用いて各ユーザーデータで処理を回す。
+--}}
+@foreach ($users as $user)
+<h3>Users Data</h3>
+{{-- ユーザー名とメールアドレスを表示 --}}
+<p>{{ $user->name }}</p>
+<p>{{ $user->email }}</p>    
+<p>ーーーーーーーーーーー</p>
+@endforeach
